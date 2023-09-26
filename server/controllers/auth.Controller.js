62a15ea60a7c088424b89e518c4dbdd62b589667
SUNIL SURENDRAN <133184647+SunilSurendran1906@users.noreply.github.com>
2023-09-26 18:46:25 +0530

    avatar bug fix

--- server/controllers/auth.Controller.js
+++ server/controllers/auth.Controller.js
@@ -11,10 +11,9 @@ exports.registerUser = catchAsyncError(async (req, res, next) => {
   let avatar;
 
   let BASE_URL = process.env.BACKEND_URL;
-  if (process.env.NODE_ENV === "production") {
+  if (process.env.NODE_ENV == "production") {
     BASE_URL = `${req.protocol}://${req.get("host")}`;
   }
-
   if (req.file) {
     avatar = `${BASE_URL}/Uploads/user/${req.file.originalname}`;
   }
@@ -169,7 +168,8 @@ exports.updateProfile = catchAsyncError(async (req, res, next) => {
   console.log(typeof process.env.NODE_ENV, process.env.NODE_ENV);
   if (process.env.NODE_ENV == "production") {
     BASE_URL = `${req.protocol}://${req.get("host")}`;
-  } else if (req.file) {
+  }
+  if (req.file) {
     avatar = `${BASE_URL}/Uploads/user/${req.file.originalname}`;
     newUserData = { ...newUserData, avatar };
   }
